7b993cbe23c6d96eb930e58c4ab74a041566e4e5
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2023-09-17 17:53:45 +0200

    Base of order by some portfolio fields

--- src/Components/RowsStockInfoTable/index.js
+++ src/Components/RowsStockInfoTable/index.js
@@ -13,6 +13,7 @@ export default function RowsStockInfoTable(stocksInfo) {
     }
 
     function requestSort() {
+        console.log(stocksInfo);
 
     }
 
@@ -20,49 +21,48 @@ export default function RowsStockInfoTable(stocksInfo) {
 
     //const stocksInfoDestruct = stocksInfo.stocksInfo;
     return (
-            <div style={{ textAlign: '-webkit-center'}}>
-                <thead>
-                <tr>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('name')}              
-                            className="btn_header">
-                        Stock
-                        </button>
-                    </th>
-
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('name')}              
-                            className="btn_header">
-                        % of Portfolio
-                        <i className="fa fa-sort-desc" />
-                        </button>
-                    </th>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('name')}              
-                            className="btn_header">
-                        Added date
-                        </button>
-                    </th>
-                </tr>
-	            </thead>
+        <>
+            <thead>
+            <tr>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('name')}              
+                        className="btn_header">
+                    Stock
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('name')}              
+                        className="btn_header">
+                    % of Portfolio
+                    <i className="fa fa-sort-desc" />
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('name')}              
+                        className="btn_header">
+                    Added date
+                    </button>
+                </th>
+            </tr>
+            </thead>
             <tbody>
-            {stocksInfoDestruct.map((stock) => {
-                const list = (
-                <tr key={stock[0]}>
-                    <td>{stock[2]} </td>
-                    <td>{stock[15]}</td>
-                    <td>{stock[1]}</td>
-                </tr>
-                );
-                return list;
-            })}
-          </tbody>
-          </div>            
+                {stocksInfoDestruct.map((stock) => {
+                    const list = (
+                    <tr key={stock[0]}>
+                        <td>{stock[2]} </td>
+                        <td>{stock[15]}</td>
+                        <td>{stock[1]}</td>
+                    </tr>
+                    );
+                    return list;
+                })}
+            </tbody>
+        </>
     );
 }
--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import Header from '../../Components/Header';
-import HeaderStockInfoTable from '../../Components/HeaderStockInfoTable';
 import PageTitle from '../../Components/PageTitle';
 import RowsStockInfoTable from '../../Components/RowsStockInfoTable';
 import { getPortfolio } from '../../Services/portfolio.service';
@@ -11,22 +10,50 @@ export default function Portfolio(){
     console.log("Se renderiza el componente Portfolio");
     const [portfolioRecovered, setPortfolioRecovered] = useState([]);
 
+    function sortPortfolio(fieldToSort, array, sortDirection){
+
+        let field = "";
+        
+        console.log('field to sort:' , fieldToSort);
+
+
+        var date = new Date("05/31/2011");
+        console.log('la fecha es: ', date);
+        
+        array.sort((a, b) => {
+            if (fieldToSort === 'portfolioPercentage'){
+                field = 15;
+                return sortDirection === "asc" ? a[field] - b[field] : b[field] - a[field];
+            }else if (fieldToSort === 'date'){
+
+                field = 1;
+                let dayA = a[field].substring(0,2)
+                let monthA = a[field].substring(3,5)
+                let yearA = a[field].substring(6,10)
+                yearA = 20 + yearA;
+
+                let dayB = b[field].substring(0,2)
+                let monthB = b[field].substring(3,5)
+                let yearB = b[field].substring(6,10)
+                yearB = 20 + yearB;
+
+                let dateA = new Date(yearA, monthA - 1, dayA);
+                let dateB = new Date(yearB, monthB - 1, dayB);
+
+                return sortDirection === "asc" ? dateA-dateB : dateB-dateA;
+            }
+        });
+
+        //return array;
+    }
+
     useEffect(() => {
         async function fetchData() {
 
             let portfolio = [];
             let sortDirection = 'desc';
             portfolio = await getPortfolio();
-
-            console.log('portfolio recuperado: ', portfolio);
-
-            
-            portfolio.response.sort((a, b) => {
-                return sortDirection === "asc" ? a[15] - b[15] : b[15] - a[15];
-            });
-
-            console.log('portfolio recuperado ordenado: ', portfolio);
-
+            sortPortfolio('date', portfolio.response, sortDirection)
             setPortfolioRecovered(portfolio);
             
           // ...
