7819049bcf68d566c4b60e68f7655273fdc70fb9
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2023-10-10 22:46:24 +0200

    Update Trackrecord page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -8,11 +8,11 @@ export default function Home(){
           <Header></Header>
           <PageTitle title="Home"></PageTitle>
 
-          <p>
+          <p className="main_paragraph"> 
           Publicación de las estrategias de inversión de los fondos del inversionista privado AAC para los años 2021 y 2022 con objetivo de retornar beneficios en 2-3 años. 
           La información de nuestra cartera se encuentra en la sección Portfolio.
           </p>
-          <p>
+          <p className="main_paragraph"> 
           Por otro lado, en la sección Track record se muestra todo el historial de inversiones del fondo.
           </p>
     
--- src/Pages/TrackRecord/index.js
+++ src/Pages/TrackRecord/index.js
@@ -4,33 +4,42 @@ import PageTitle from '../../Components/PageTitle';
 import { getTrackrecord } from '../../Services/trackrecord.service';
 import './styles.css'
 import { TrackrecordInfoTable } from '../../Components/TrackrecordInfoTable';
+import PulseLoader from "react-spinners/PulseLoader";
 
 export default function TrackRecord(){
 
     let trackrecord = [];
     const [trackrecordInfoRecovered, setTrackrecordInfoRecovered] = useState([]);
+    const [showLoading, setshowLoading] = useState(false);
+
+    const CSSProperties = {
+        display: "block",
+        margin: "0 auto",
+        marginTop: "1em",
+    };
 
     useEffect(() => {
 
         async function fetchData() {
 
             console.log('TrackRecord() - Use effect')
-            //setshowLoading(true);
+            setshowLoading(true);
 
             try {
                 trackrecord = await getTrackrecord();
+                setTrackrecordInfoRecovered(trackrecord.response);
             }
             catch(err) {
                 console.error('TrackRecord() - catched exception: ', err)
             }
             finally {
-                
+                setshowLoading(false);
+                console.log('TrackRecord() - Use effect final: ', trackrecord)
             }
             
             
-            setTrackrecordInfoRecovered(trackrecord.response);
-            //setshowLoading(false);
-            console.log('TrackRecord() - Use effect final: ', trackrecord)
+            
+            
             
           // ...
         }
@@ -50,6 +59,14 @@ export default function TrackRecord(){
                     <TrackrecordInfoTable trackrecordInfo={trackrecordInfoRecovered}></TrackrecordInfoTable>
                 }
             </table>
+            <PulseLoader
+                color={"#3CC16A"}
+                loading={showLoading}
+                size={25}
+                cssOverride={CSSProperties}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
         </div>
     );
 }
