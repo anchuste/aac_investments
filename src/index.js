529b77ccfb3459541a4c8bcf86738491d3ee6a33
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2024-02-08 23:41:15 +0100

    Update Bootstrap dependencies and Navbar styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+//import "bootstrap.bundle.min"
 import './index.css';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
