49b9f3418c89c0bd5dc7a8164c5b8736de76237a
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2024-02-13 20:56:15 +0100

    Add i18next and react-i18next dependencies

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './Resources/translation/i18n';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
